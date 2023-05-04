a1bf174ad22a14b2edcef9206a67b8a0dcd3247a
Adrián Freire Torres <adrianft25@gmail.com>
2023-05-04 16:10:56 +0200

    Pruebas con modulos

---
+++ funciones.py
@@ -0,0 +1,5 @@
+def sum_values(n1, n2, n3):
+    print(n1 + n2 + n3)
+
+def imprimirValor(value):
+    print(value)
---
+++ modulos.py
@@ -0,0 +1,17 @@
+import funciones
+# from funciones import sum_values, imprimirValor
+
+# sum_values(2,2,4)
+# imprimirValor("Hola Python!")
+
+funciones.sum_values(2,2,4)
+funciones.imprimirValor("Hola Python!")
+
+import math
+
+print(math.pi)
+print(math.pow(2,8))
+
+from math import pi as pi_value
+
+print(pi_value)
