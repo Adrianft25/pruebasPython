4e971abcd15ae739b6d3a3b8a5b9d77c894d0b26
Adrián Freire Torres <adrianft25@gmail.com>
2023-05-10 19:13:43 +0200

    Pruebas de dates terminadas

--- Intermedio/dates.py
+++ Intermedio/dates.py
@@ -22,4 +22,50 @@ print_date(y2024)
 
 from datetime import time
 
-currentTime = time()
+currentTime = time(22,5,0)
+
+print(currentTime.hour)
+print(currentTime.minute)
+print(currentTime.second)
+
+from datetime import date
+
+currentDate = date.today()
+
+print(currentDate.year)
+print(currentDate.month)
+print(currentDate.day)
+
+currentDate2 = date(1998,10,14)
+
+print(currentDate2.year)
+print(currentDate2.month)
+print(currentDate2.day)
+
+
+# Operaciones con dates
+
+currentDate3 = date(currentDate2.year + 4, currentDate2.month - 1, currentDate2.day) 
+
+print(currentDate3.year)
+print(currentDate3.month)
+print(currentDate3.day)
+
+# Operaciones entre datetimes y times
+
+diff = y2024 - now
+print(diff)
+
+diff2 = y2024.date() - currentDate3
+print(diff2)
+
+from datetime import timedelta
+
+start = timedelta(200,100,250,weeks=10)
+end = timedelta(300,100,250,weeks=27)
+
+print(end - start)
+print(end + start)
+# NO SE PUEDE HACER         print(end * start)
+# SI, PERO NO TIENE SENTIDO print(end / start)
+
